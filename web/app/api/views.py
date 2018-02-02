795761026259e1dc534f8e962751dba9ae33536e
Linus Törngren <linus@etnolit.se>
2018-02-02 15:41:43 +0100

    WIP

--- web/app/api/views.py
+++ web/app/api/views.py
@@ -2,7 +2,9 @@ from flask.views import MethodView
 from flask import current_app, jsonify, abort
 from flask_login import login_required, current_user
 from . import api
+from .. import db
 from ..models import Account
+import uuid
 
 
 class AccountView(MethodView):
@@ -14,23 +16,41 @@ class AccountView(MethodView):
             # List all resources
             accounts = []
             for a in current_user.accounts:
-                accounts.append({ 'id': a.number, 'number': a.number, 'balance': a.balance })
-            return { 'data': accounts }
+                accounts.append({ 'id': a.number, 'number': a.number, 'balance': a.balance})
+            return {'data': accounts}
         else:
             # Return specific resource
             account = Account.query.filter_by(number=account_id).one()
             if account is None:
                 return {'error': 'Not Found.'}, 404
-            return {'data': {'id': account.number, 'number': account.number, 'balance': account.balance }}
+            return {'data': {'id': account.number, 'number': account.number, 'balance': account.balance}}
 
 
     def post(self):
         """Create account."""
-        pass
+        candidate = str(int(uuid.uuid4()))[-8:]
+        while not Account.query.filter_by(number=candidate).first() is None:
+            candidate = str(int(uuid.uuid4()))[-8:]
+
+        account = Account(number=candidate, balance=0, holder=current_user)
+        db.session.add(account)
+        db.session.commit()
+        current_app.logger.info('Created account ({}).'.format(account.id))
+
+        return {'data': {'id': account.number, 'number': account.number, 'balance': account.balance}}
 
     def delete(self, account_id):
         """Remove account."""
-        pass  # Delete resource
+        account = Account.query.filter_by(number=account_id).one()
+
+        if account is None:
+            return {'error': 'Not Found.'}, 404
+
+        db.session.delete(account)
+        db.session.commit()
+        current_app.logger.info('Deleted account ({})'.format(account.id))
+
+        return {'data': {'id': account.number}}
 
     def put(self, account_id):
         """Transfer funds?"""
@@ -38,9 +58,9 @@ class AccountView(MethodView):
 
 
 account_view = AccountView.as_view('accounts')
-api.add_url_rule('/accounts/', defaults={'account_id': None},
+api.add_url_rule('/accounts', defaults={'account_id': None},
                  view_func=account_view, methods=['GET'])
-api.add_url_rule('/accounts/', view_func=account_view, methods=['POST'])
+api.add_url_rule('/accounts', view_func=account_view, methods=['POST'])
 api.add_url_rule('/accounts/<string:account_id>', view_func=account_view, methods=['GET', 'PUT', 'DELETE'])
 
 
@@ -69,7 +89,7 @@ class StockView(MethodView):
 
 
 stock_view = StockView.as_view('stock_view')
-api.add_url_rule('/stocks/', defaults={'stock_id': None},
+api.add_url_rule('/stocks', defaults={'stock_id': None},
                  view_func=stock_view, methods=['GET'])
 api.add_url_rule('/stocks/<string:stock_id>', view_func=stock_view, methods=['GET'])
 
